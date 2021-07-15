fcd6e6229ae7e96b8508a67bfb18a514daaa3f2a
Kovacs Jacint <jacint1998@gmail.com>
2021-07-15 19:37:13 +0300

    thread folder added

---
+++ src/com/sda/threads/ThreadGc.java
@@ -0,0 +1,11 @@
+package com.sda.threads;
+
+public class ThreadGc implements Runnable {
+
+    @Override
+    public void run() {
+        System.gc();
+        System.runFinalization();
+        System.out.println("garbage collector has been invoked");
+    }
+}
