dbfef6e15249b3b4fd429eff676cd6b80d61d053
Kovacs Jacint <jacint1998@gmail.com>
2021-07-21 11:49:46 +0300

    few changes and stream methods tested

--- src/com/sda/streams/Main.java
+++ src/com/sda/streams/Main.java
@@ -16,25 +16,37 @@ public class Main {
         List<String> courses = List.of(
                 "Spring", "Spring Boot", "API" , "Microservices", "AWS", "PCF","Azure", "Docker", "Kubernetes"
         );
-        courses.stream().forEach(word -> System.out.println(word));
+        courses.stream()
+                .forEach(word -> System.out.println(word));
         System.out.println("=========================");
+
         courses.stream()
                 .filter(word -> word.contains("Spring"))
                 .forEach(word -> System.out.println(word));
         System.out.println("-------------------------");
+
         courses.stream()
                 .filter(word -> word.length()>4)
                 .forEach(word -> System.out.println(word));
         System.out.println("-------------------------");
+
         courses.stream()
                 .forEach(word -> System.out.println(word.length()));
         System.out.println("-------------------------");
-        courses.stream()
-                .sorted((word1, word2) -> Integer.compare(word1.length(),word2.length()))
-                .forEach(word -> System.out.println(word));
-        System.out.println("-------------------------");
+
         courses.stream()
                 .sorted((word1, word2) -> Integer.compare(word2.length(),word1.length()))
                 .forEach(word -> System.out.println(word));
+        System.out.println("-------------------------");
+
+        courses.stream()
+                .sorted((word1, word2) -> Integer.compare(word1.length(),word2.length()))
+                .forEach(word -> System.out.println(word));
+        System.out.println("--------------------------");
+
+        courses.stream()
+                .limit(5)
+                .forEach(word -> System.out.println(word));
+        System.out.println("--------------------------");
     }
 }
